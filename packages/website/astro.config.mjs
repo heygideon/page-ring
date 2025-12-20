7141413d7f4dd68a7f9b1c920387159f0ff6dacd
heygideon <47083178+heygideon@users.noreply.github.com>
2025-12-20 15:37:26 +0000

    fix: remove node adapter

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import node from "@astrojs/node";
-
 import mdx from "@astrojs/mdx";
 
 import cloudflare from "@astrojs/cloudflare";
