c50b37db1e9c975bb7c2b38e4c0a791bc21c6907
heygideon <47083178+heygideon@users.noreply.github.com>
2026-01-05 02:19:21 +0000

    fix(embed): feature flag v2

--- packages/embed/src/embed.ts
+++ packages/embed/src/embed.ts
@@ -1,7 +1,12 @@
 import "./Overlay.svelte";
-import "./Link_v2.svelte";
 
 import styles from "./styles/fonts.css?url";
+
+if (window.location.hash.includes("pagering-v2")) {
+  import("./Link_v2.svelte");
+} else {
+  import("./Link.svelte");
+}
 
 const overlay = document.createElement("pagering-overlay");
 document.body.append(overlay);
